3551c4fecc3ef65812906564d478aac93c7b166a
aitorgu <aitor.icj@gmail.com>
2024-10-22 09:27:09 +0200

    Añadir nuevos componentes a través de librerías

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    //implementation(com.google.android.material:material:1.2.1)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
